772a080e292f1ff796e1b62649b3cfeb5271673a
rhodeon <ruonabraks@gmail.com>
2021-05-18 03:12:03 +0100

    Refactored code to recognized flags for different operations.

--- tagmage.py
+++ tagmage.py
@@ -3,58 +3,26 @@
 """
 
 import sys
-import os
-import eyed3
 
-def set_working_dir():
-    """
-        Changes the working directory to source.
-    """
-    wrkdir = sys.argv[1]        # source path specified in command
-    os.chdir(wrkdir)
-    
-def obtain_dest_dir():
-    """
-        Sets the destination directory to place files.
-    """
-    dest_dir = sys.argv[2]      # destination path specified in command
-    return dest_dir
+from sort_files import *
 
-
-def list_files():
-    """
-        Creates a list of files in source directory
-    """
-    files = [f for f in os.listdir() if not f.startswith(".")]      # ignore hidden files
-    return files
+flags = ["--sort", "--merge"]   # list of valid argument flags
 
 def main():
-    """
-        Creates folders and move files
-    """
-    dest_dir = obtain_dest_dir()    # destination directory
-    raw_files = list_files()        # files in source directory
+    args = sys.argv
+    flag = args[1]
+    
+    if flag not in flags:
+        print("Enter a valid flag")
+        return
 
-    for raw_name in raw_files:
-        tagged_file = eyed3.load(raw_name)      # eyed3 object for tag info
-        title_name = tagged_file.tag.title      # title tag
-        artist_dir = tagged_file.tag.artist     # artist tag
-        album_dir = tagged_file.tag.album       # album tag
+    src_dir = args[2]
+    set_working_dir(src_dir)
+    raw_files = list_files()    # files in source directory
 
-        # skip file is either title, artist or album is missing
-        if not title_name or not artist_dir or not album_dir:
-            continue
-
-        new_directory = os.path.join(dest_dir, artist_dir, album_dir)   # full path of directory to move file
-        new_filename = os.path.join(new_directory, title_name) + ".mp3"     # new properly tagged name of file
-
-        if not os.path.exists(new_directory):
-            os.makedirs(new_directory)
-
-        if not os.path.exists(new_filename):
-            os.rename(raw_name, new_filename) 
-
+    if flag == "--sort":
+        dest_dir = args[3]
+        sort_files(raw_files, dest_dir)
+    
 if __name__ == "__main__":
-    set_working_dir()
-    obtain_dest_dir()
     main()
