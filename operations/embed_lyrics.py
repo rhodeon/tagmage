18f41b1cbcd2f50cc3d79ca3a809bc120646cba9
rhodeon <ruonabraks@gmail.com>
2021-05-20 12:16:53 +0100

    Added exception handling and result displaying when embeding lyrics.

--- operations/embed_lyrics.py
+++ operations/embed_lyrics.py
@@ -3,6 +3,9 @@ import eyed3
 
 from utils import *
 
+successes = []
+failures = []
+
 def embed_lyrics(songs):
     """
         Embeds synchronized lyrics files in songs with corresponding names
@@ -12,10 +15,32 @@ def embed_lyrics(songs):
         lyrics = os.path.splitext(song)[0] + ".lrc"
 
         if os.path.exists(lyrics):  # corresponding lyrics file exists
-            with open(lyrics) as lrc:
-                lyrics_string = lrc.read()
-            
-            tagged_song = eyed3.load(song)
-            tagged_song.tag.lyrics.set(text = lyrics_string)
-            tagged_song.tag.save()
-            print(song)     # Displays the songs affected to the user
+            try:
+                with open(lyrics) as lrc:
+                    lyrics_string = lrc.read()
+                
+                tagged_song = eyed3.load(song)
+                tagged_song.tag.lyrics.set(text = lyrics_string)    # embed lyrics
+                os.remove(lyrics)   # delete lyrics file
+                
+                tagged_song.tag.save()
+                successes.append(song)
+
+            except:
+                failures.append(song)
+                continue
+
+    displayResult()
+
+def displayResult():
+    if successes:
+        print("SUCCESSFUL:")
+        for song in successes:
+            print(song)
+        print()
+
+    if failures:
+        print("FAILED:")
+        for song in failures:
+            print(song)
+        print()
